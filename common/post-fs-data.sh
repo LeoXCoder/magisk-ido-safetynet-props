70a0e1f48a99ca80d3e806457c3f2651625e7589
Ruslan Gaysin <RGaysin@gmail.com>
2017-10-31 09:57:15 +0300

    Fix to ido

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -12,29 +12,25 @@ if [ ! -f $resetprop ]; then
     resetprop="/data/magisk/magisk resetprop"
 fi
 
-$resetprop ro.boot.verifiedbootstate green
-$resetprop ro.boot.veritymode enforcing
+#$resetprop ro.boot.verifiedbootstate green
+#$resetprop ro.boot.veritymode enforcing
 
 # https://forum.xda-developers.com/showpost.php?p=70831797&postcount=3665
-$resetprop ro.build.description "gemini-user 6.0.1 MXB48T V8.1.2.0.MAAMIDI release-keys"
-$resetprop ro.build.fingerprint "Xiaomi/gemini/gemini:6.0.1/MXB48T/V8.1.2.0.MAAMIDI:user/release-keys"
+$resetprop ro.build.description "ido-user 5.1.1 LMY47V V8.1.3.0.LAIMIDI release-keys"
+$resetprop ro.build.fingerprint "Xiaomi/ido/ido:5.1.1/LMY47V/V8.1.3.0.LAIMIDI:user/release-keys"
 
-$resetprop ro.bootimage.build.fingerprint "Xiaomi/gemini/gemini:6.0.1/MXB48T/V8.1.2.0.MAAMIDI:user/release-keys"
+$resetprop ro.bootimage.build.fingerprint "Xiaomi/ido/ido:5.1.1/LMY47V/V8.1.3.0.LAIMIDI:user/release-keys"
 
-$resetprop selinux.reload_policy 1
+#$resetprop selinux.reload_policy 1
 
 # https://www.reddit.com/r/pokemongodev/comments/58gnq3/root_bypass_the_new_update_leads_repost/
-$resetprop ro.build.selinux 0
+# $resetprop ro.build.selinux 0
 
 ## https://forum.xda-developers.com/showpost.php?p=71154562&postcount=4
-$resetprop ro.debuggable 0
-$resetprop ro.secure 1
-$resetprop ro.build.type user
-$resetprop ro.build.tags release-keys
+#$resetprop ro.debuggable 0
+#$resetprop ro.secure 1
+#$resetprop ro.build.type user
+#$resetprop ro.build.tags release-keys
 
 # https://github.com/topjohnwu/MagiskManager/issues/112#issuecomment-280993699
-if [ -d "/magisk/.core/magiskhide" ]; then
-  sh /magisk/.core/magiskhide/enable
-else
-  /data/magisk/magisk magiskhide --enable
-fi
+sh /magisk/.core/magiskhide/enable
